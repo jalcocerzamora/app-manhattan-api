1849b55299f049a7f8b5a1a7ca15878dce9ec755
JonAlco <alco_1993@hotmail.com>
2021-02-12 17:06:59 -0500

    .

--- app/db/models/index.js
+++ app/db/models/index.js
@@ -12,11 +12,11 @@ const config = require(__dirname + '/../../config/config.json')[NODE_ENV.concat(
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+// }
 
 fs
   .readdirSync(__dirname)
