0afe0801b0f614c32bc1e39f324dd5d91ad21fb9
JonAlco <alco_1993@hotmail.com>
2021-02-12 15:49:38 -0500

    server.js

--- server.js
+++ server.js
@@ -1,28 +1,50 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config({ path: __dirname + '/.env' })
-}
+// if NODE_ENV value not define then dev value will be assign 
+mode = process.env.NODE_ENV || 'default';
 
+const config      = require('config');
+const pack        = require('./package');
 const bodyParser  = require('body-parser');
+const chalk       = require('chalk');
+const swaggerUi   = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+const configAPI   = require('./app/config/api.config');
+const configCORS  = require('./app/config/cors.config');
+const configSSL   = require('./app/config/ssl.config');
+
+// if (process.env.NODE_ENV !== 'production') { require('dotenv').config({ path: __dirname + '/.env' }) }
+
 const express     = require('express');
 const http        = require('http');
 const https       = require('https');
 const cross       = require("cors");
-const configAPI   = require('./app/config/api.config');
-const configCORS  = require('./app/config/cors.config');
-const configSSL = require('./app/config/ssl.config');
 
 const app = express();
 const server = (configSSL.valid ? https.createServer(configSSL.options, app) : http.createServer(app));
 const middleware = require('./app/config/middleware.config')(app);
-const PORT = parseInt(process.env.PORT, 10) || 8000;
+// const PORT = parseInt(process.env.PORT, 10) || 8000;
 
-server.listen(PORT, () => { console.log('Server is listening to port %d', PORT) });
+const start = () => (
+  // server.listen(PORT, () => { console.log('Server is listening to port %d', PORT) });
+  app.listen(config.port, () => {
+    console.log(chalk.yellow('.......................................'));
+    console.log(chalk.green(config.name));
+    console.log(chalk.green(`Port:\t\t${config.port}`));
+    console.log(chalk.green(`Mode:\t\t${config.mode}`));
+    console.log(chalk.green(`App version:\t${pack.version}`));
+    console.log(chalk.green("database connection is established"));
+    console.log(chalk.yellow('.......................................'));
+  })
+);
 
-app.set('private-key', configAPI.PRIVATE_KEY)
-app.use(express.static(process.env.STATIC_DIR));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.set('private-key', config.api.PRIVATE_KEY)
+// app.use(express.static(process.env.STATIC_DIR));
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cross(configCORS.options));
 
 // require('./app/websocket')(app, middleware);
+
 require('./app/routes')(app, middleware);
+
+start();
